18df4620a1d439a81d4e7ff09b429a86b94bc310
Arthur Schiwon <blizzz@arthur-schiwon.de>
2018-05-18 17:19:58 +0200

    add note about what happens on button clicks
    thx to @jospoortvliet for human understandable text
    
    Signed-off-by: Arthur Schiwon <blizzz@arthur-schiwon.de>

--- templates/Settings/personal.php
+++ templates/Settings/personal.php
@@ -29,6 +29,7 @@ script('data_request', ['init', 'app']);
 
 <div id="data-request" class="section">
 	<h2><?php p($l->t('Account')); ?></h2>
+	<p><?php p($l->t('You can request an export of your data or account deletion from the system administrator. This can take up to 30 days to be executed.')); ?></p>
 	<div>
 		<button id="data-request-export" data-request="export" class="button"><?php p($l->t('Request data export')); ?></button><span class="warning hidden"></span>
 	</div>
