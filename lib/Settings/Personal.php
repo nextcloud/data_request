323dcfded9d73d0292c9637c5d13c8e5cb6a5b91
Arthur Schiwon <blizzz@arthur-schiwon.de>
2022-11-11 20:08:32 +0100

    add return type hints
    Signed-off-by: Arthur Schiwon <blizzz@arthur-schiwon.de>

--- lib/Settings/Personal.php
+++ lib/Settings/Personal.php
@@ -27,32 +27,15 @@ use OCP\AppFramework\Http\TemplateResponse;
 use OCP\Settings\ISettings;
 
 class Personal implements ISettings {
-
-	/**
-	 * @return TemplateResponse returns the instance with all parameters set, ready to be rendered
-	 * @since 9.1
-	 */
-	public function getForm() {
+	public function getForm(): TemplateResponse {
 		return new TemplateResponse('data_request', 'Settings/personal', [], '');
 	}
 
-	/**
-	 * @return string the section ID, e.g. 'sharing'
-	 * @since 9.1
-	 */
-	public function getSection() {
+	public function getSection(): string {
 		return 'personal-info';
 	}
 
-	/**
-	 * @return int whether the form should be rather on the top or bottom of
-	 * the admin section. The forms are arranged in ascending order of the
-	 * priority values. It is required to return a value between 0 and 100.
-	 *
-	 * E.g.: 70
-	 * @since 9.1
-	 */
-	public function getPriority() {
+	public function getPriority(): int {
 		return 80;
 	}
 }
