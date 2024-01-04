dccbb2c791908cd184aada9780b5f6f18485f9f0
Max <max@nextcloud.com>
2024-01-04 11:43:32 +0100

    test(cy): test warning when no admin email set
    Signed-off-by: Max <max@nextcloud.com>

--- cypress/e2e/app.cy.js
+++ cypress/e2e/app.cy.js
@@ -1,12 +1,21 @@
 import { User } from '@nextcloud/cypress'
 
-describe('The app', function() {
-	it('is visible in the settings', function() {
-		const user = new User('admin', 'admin')
-		cy.login(user)
-		cy.visit('/settings/user')
-		cy.get('#app-content').scrollTo('bottom')
-		cy.get('#data-request-export').should('be.visible')
-		cy.get('#data-request-deletion').should('be.visible')
+const user = new User('admin', 'admin')
+
+for (const action of ['export', 'deletion']) {
+	describe(action, function() {
+		it('is visible in the settings', function() {
+			cy.login(user)
+			cy.visit('/settings/user')
+			cy.appContent().scrollTo('bottom')
+			cy.getButton(action).should('be.visible')
+		})
+
+		it('warns the user about missing admin emails', function() {
+			cy.login(user)
+			cy.triggerRequest(action)
+			cy.getWarning(action).should('contain', 'email')
+		})
+
 	})
-})
+}
